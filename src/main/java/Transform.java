cbd87ed055e81b97d45ada21807b76a98afca18b
eliaswilcos1 <elias.wilcoski@gmail.com>
2018-02-28 21:11:23 -0600

    Started MP4

--- src/main/java/Transform.java
+++ src/main/java/Transform.java
@@ -1,2 +1,173 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Transform Class.
+ */
 public class Transform {
+    /**
+     * default color shift.
+     */
+    static int DEFAULT_COLOR_SHIFT;
+    /**
+     * default position shift.
+     */
+    static int DEFAULT_POSITION_SHIFT;
+    /**
+     * default resize amount
+     */
+    static int DEFAULT_RESIZE_AMOUNT;
+    /**
+     * default constructor
+     */
+    public Transform() {
+    }
+    /**
+     * expand horizontal
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] expandHorizontal(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * expand vertical
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] expandVertical(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * flip horizontal
+     * @param originalImage original image
+     */
+    static RGBAPixel[][] flipHorizontal(final RGBAPixel[][] originalImage) {
+    }
+    /**
+     * flip vertical
+     * @param originalImage original image
+     */
+    static RGBAPixel[][] flipVertical(final RGBAPixel[][] originalImage) {
+    }
+    /**
+     * green screen
+     * @param originalImage original image
+     */
+    static RGBAPixel[][] greenScreen(final RGBAPixel[][] originalImage) {
+    }
+    /**
+     * less alpha
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] lessAlpha(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * less blue
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] lessBlue(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * less green
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] lessGreen(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * less red
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] lessRed(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * more alpha
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] moreAlpha(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * more blue
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] moreBlue(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * more green
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] moreGreen(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * more red
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] moreRed(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * mystery
+     * @param originalImage original image
+     */
+    static RGBAPixel[][] mystery(final RGBAPixel[][] originalImage) {
+    }
+    /**
+     * rotate left
+     * @param originalImage original image
+     */
+    static RGBAPixel[][] rotateLeft(final RGBAPixel[][] originalImage) {
+    }
+    /**
+     * rotate right
+     * @param originalImage original image
+     */
+    static RGBAPixel[][] rotateRight(final RGBAPixel[][] originalImage) {
+    }
+    /**
+     * shift down
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] shiftDown(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * shift left
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] shiftLeft(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * shift right
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] shiftRight(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * shift up
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] shiftUp(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * shrink horizontal
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] shrinkHorizontal(final RGBAPixel[][] originalImage, final int amount) {
+    }
+    /**
+     * shrink vertical
+     * @param originalImage original image
+     * @param amount amount
+     */
+    static RGBAPixel[][] shrinkVertical(final RGBAPixel[][] originalImage, final int amount) {
+    }
 }
